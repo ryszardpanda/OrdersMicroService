b129c00d56b9143654dffdfb69f95f3bddd682f7
ryszardpanda <rychu48@op.pl>
2025-04-28 19:25:29 +0200

    +initalization of spring

---
+++ src/test/java/com/Orders/OrdersMicroService/OrdersMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Orders.OrdersMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
