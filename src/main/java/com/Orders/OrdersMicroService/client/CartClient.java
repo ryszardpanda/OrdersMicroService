0f7e5a4d8ed9cc3a5b60fadbb882a9944a63fd38
ryszardpanda <rychu48@op.pl>
2025-06-17 22:45:02 +0200

    +fixed micro-service name in connection string

--- src/main/java/com/Orders/OrdersMicroService/client/CartClient.java
+++ src/main/java/com/Orders/OrdersMicroService/client/CartClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "cart-microservice", url = "${base.url.cart-micro-service}")
+@FeignClient(value = "cart-microservice", url = "${base.url.cart-microservice}")
 public interface CartClient {
 
     @GetMapping("/{id}")
